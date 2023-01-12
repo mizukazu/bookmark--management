238c597928ac143b373036ba7b1f26652be5d920
MizunoKazuki <mizukazu.work@gmail.com>
2023-01-12 17:20:29 +0900

    configの読み込みパスを修正

--- config/config.php
+++ config/config.php
@@ -16,9 +16,15 @@ class Config {
    * 設定ファイルを読み込むメソッド
    */
   private function getConfig() {
-    $json = mb_convert_encoding(file_get_contents(__DIR__ . '/config.json'), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-    $config = json_decode($json, true);
-    self::$config = $config;
+    // $json = mb_convert_encoding(file_get_contents(__DIR__ . '/config.json'), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+    // $config = json_decode($json, true);
+    // self::$config = $config;
+    // var_dump($config);
+    // echo  __DIR__ . '/data/data.json';
+    // echo  __DIR__ . '/icon/icon.json';
+    self::$config['url']['data_url'][0] = __DIR__ . '/data/data.json';
+    self::$config['url']['icon_url'] = __DIR__ . '/icon/icon.json';
+    // var_dump(self::$config['url']['data_url'][0]);
   }
 }
 Config::init();
